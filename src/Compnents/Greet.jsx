c49156694756f838984a5ab97ef330d247a5ebb3
Bikash Kumar <Kbikash845@gmail.com>
2024-06-21 11:30:38 -0700

    first commit

---
+++ src/Compnents/Greet.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Greet(){
+    return(
+        <div>
+            <h2>Vicky kumar</h2>
+        </div>
+    )
+}
+export default Greet;
---
+++ src/Compnents/Welcome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Welcome extends Component{
+  render(){
+    return(
+        <div>
+          <h3>Bikash kumar</h3>
+        </div>
+    )
+  }
+};
+
+export default Welcome;
